a9d41c72a8eb1a1921116914076ff4734e978d07
Prashant Bhargava <prashant@artifacia.com>
2017-06-17 03:09:32 +0530

    Added VideoList component

--- src/index.js
+++ src/index.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list'
 
 const YOUTUBE_API_KEY = 'API_KEY';
 
+
 // Create a new component. This component should produce
 // some HTML
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key: YOUTUBE_API_KEY, term: "youtube"}, (videos) => {
+            this.setState({ videos });  // this.setState({ videos: videos })
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
 }
 
 // Take this components' generate HTML and put it on
