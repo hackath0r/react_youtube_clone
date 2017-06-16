eaabcfa9e9478960d2c66af55fc22612e685971c
Prashant Bhargava <prashant@artifacia.com>
2017-06-17 04:10:04 +0530

    video iframe and details added

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list'
+import VideoDetail from './components/video_detail'
 
 const YOUTUBE_API_KEY = 'API_KEY';
 
@@ -25,6 +26,7 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
+                <VideoDetail video={this.state.videos[0]}/>
                 <VideoList videos={this.state.videos} />
             </div>
         );
